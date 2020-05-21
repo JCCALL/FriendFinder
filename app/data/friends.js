cc00532ac3dfe81d3c6c93a4baae8f824e0b0c16
Dark Knight <jordanccall@gmail.com>
2020-05-21 16:32:07 -0600

    started the survey, finished the friends.js file

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,47 +1,47 @@
 var friends = [
     {
-        name: "",
-        photo: "",
+        name: "Tom",
+        photo: "https://i.pinimg.com/originals/92/a6/2f/92a62f0221f58fe503a15fcb13f5c107.png",
         scores: [1,1,1,1,1,1,1,1,1,1]
     },
     {
-        name: "",
-        photo: "",
+        name: "Scooby Doo",
+        photo: "https://i.redd.it/qqr4nhy7pd331.jpg",
         scores: [5,1,4,4,5,1,2,5,4,1]
     },
     {
-        name: "",
-        photo: "",
+        name: "Yogi Bear & Boo Boo",
+        photo: "https://i.pinimg.com/originals/7e/c3/89/7ec389588da7fd199456b61b5a14ce01.jpg",
         scores: [3,2,2,4,5,2,1,3,4,3]
     },
     {
-        name: "",
-        photo: "",
+        name: "Dino",
+        photo: "https://i.pinimg.com/originals/39/5f/ce/395fce08f4e9c7078da6e31ded2e1292.jpg",
         scores: [1,5,3,3,2,1,4,1,2,3]
     },
     {
-        name: "",
-        photo: "",
+        name: "Pluto",
+        photo: "https://i.pinimg.com/originals/84/e6/52/84e65278d7abbb61ea2201756ce3ebd5.jpg",
         scores: [4,4,4,2,2,5,2,5,2,1]
     },
     {
-        name: "",
-        photo: "",
+        name: "Toothless",
+        photo: "https://i.pinimg.com/originals/b8/71/3c/b8713c5bd37a60f5e40235e6e2053f10.png",
         scores: [4,2,2,3,4,1,2,2,2,3]
     },
     {
-        name: "",
-        photo: "",
+        name: "Pikachu",
+        photo: "https://i.pinimg.com/originals/f5/1d/08/f51d08be05919290355ac004cdd5c2d6.png",
         score: [5,5,5,5,5,5,5,5,5,5]
     },
     {
-        name: "",
-        photo: "",
+        name: "Brian Griffin",
+        photo: "https://i.pinimg.com/originals/8f/a9/11/8fa91183ac378ec914a1f1f83a0df941.jpg",
         score: [4,2,3,1,2,3,1,2,3,2]
     },
     {
-        name: "",
-        photo: "",
+        name: "Taz",
+        photo: "https://i.pinimg.com/originals/d2/b8/17/d2b8170741c33ee9ef49b877824eb5c4.jpg",
         score: [2,5,1,3,5,2,3,3,1,4]
     }
 ];
