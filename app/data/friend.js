6b3e816c3d2d6a0185cc5426eb84b9665394c495
Dark Knight <jordanccall@gmail.com>
2020-05-21 11:12:00 -0600

    added array of objects in the friend.js

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,3 +1,49 @@
-
+var friends = [
+    {
+        name: "",
+        photo: "",
+        scores: [1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "",
+        photo: "",
+        scores: [5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        name: "",
+        photo: "",
+        scores: [3,2,2,4,5,2,1,3,4,3]
+    },
+    {
+        name: "",
+        photo: "",
+        scores: [1,5,3,3,2,1,4,1,2,3]
+    },
+    {
+        name: "",
+        photo: "",
+        scores: [4,4,4,2,2,5,2,5,2,1]
+    },
+    {
+        name: "",
+        photo: "",
+        scores: [4,2,2,3,4,1,2,2,2,3]
+    },
+    {
+        name: "",
+        photo: "",
+        score: [5,5,5,5,5,5,5,5,5,5]
+    },
+    {
+        name: "",
+        photo: "",
+        score: [4,2,3,1,2,3,1,2,3,2]
+    },
+    {
+        name: "",
+        photo: "",
+        score: [2,5,1,3,5,2,3,3,1,4]
+    }
+]
 
 module.exports = friends;
